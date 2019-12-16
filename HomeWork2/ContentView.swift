dd95599c16954eff38f730d5fdcc76e96b39f622
Pavel Antonov <p.antonov@securitycode.ru>
2019-12-16 09:43:39 +0300

    Initial Commit

---
+++ HomeWork2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork2
+//
+//  Created by Pavel Antonov on 16.12.2019.
+//  Copyright © 2019 OtusCourse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
